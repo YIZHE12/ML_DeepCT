830c3660940dc44b9023e760fa26e8984c254ef8
YIZHE12 <YIZHE12@users.noreply.github.com>
2019-03-27 17:29:08 +0100

    Update Export_tiff.m

--- Nikon/Export_tiff.m
+++ Nikon/Export_tiff.m
@@ -6,8 +6,6 @@
 % the imagesize is given by nImage,mImage,lImage (size in .vgi file)
 % Read the VOL file based on the given image size and output tif file to
 % the new folder 'Recon_tif + sample name'
-% Created: 17/6-2015 by Carsten Gundlach (cagu@fysik.dtu.dk)
-% Edited: 4/10-2018 by Yi Zheng (yizhe@fysik.dtu.dk)
 clear all
 close all
 clc
@@ -30,26 +28,17 @@ mkdir (Foldername)
 Foldername = strcat(Foldername,'\',fileList.name(1:end-4));
 %%
 tic
-% FileStem='H:\Hay\Compact\Hay_120kV_11W_air_01\Hay_120kV_11W_air'
-% FileImportName=[FileStem '.vol']
-% size=[780 765 982];
-% nImage=size(1);
-% mImage=size(2);
-% mImage=size(3);
+
 fileList = dir('*.vol');
 file_name_vol=fileList.name;
 fid = fopen(file_name_vol);
 colormap(gray)
 for i=1:lImage
     A = fread(fid, nImage*mImage*1, 'float');
-    vol = reshape(A, [nImage, mImage, 1]);
-%     figure
-%     imshow(vol,[])
-    
+    vol = reshape(A, [nImage, mImage, 1]);    
     vol2 = uint16(vol*100);
     NewFileName=sprintf('%s_convert_%04d.tif',Foldername,i);
     imwrite(vol2(:,:,1),NewFileName,'tif')
-%    i
 end
 fclose(fid)
 toc
